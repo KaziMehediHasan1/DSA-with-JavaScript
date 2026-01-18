40effa6eefc3bb6386c766d13d212f9ed8c23af5
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-01-18 23:01:49 +0600

    recursion: solved a factorial prob. using recursion formula

---
+++ Recursion/Factorial.js
@@ -0,0 +1,14 @@
+// Identify Base Case
+//  figure out the simplest possible input for which you can directly return a result without further recursion.
+//
+
+function factorial(n) {
+  // Base case: if n is 0, return 1
+  if (n === 1) return 1;
+  // return n * factorial(n - 1);
+  for (let i = 1; i < n; i++) {
+    n = n + i;
+  }
+}
+
+console.log(factorial(5));
